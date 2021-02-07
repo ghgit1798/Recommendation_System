21947b6596e8b1e5e0589408af837ab2f13f4091
ghgoo1798 <ghgoo1798@gamil.com>
2021-02-08 04:20:10 +0900

    settings.py static

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_DIR = os.path.dirname(BASE_DIR)
+STATIC_DIR = os.path.dirname(BASE_DIR, 'beer/static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
